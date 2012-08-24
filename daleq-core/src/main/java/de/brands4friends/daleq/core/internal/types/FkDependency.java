99a0139d7f45d6ffc6cc5abee1c8f8e7a992fb92
Lars Girndt <lars.girndt@brands4friends.de>
2012-08-24 22:26:53 +0200

    added the Graph's Edge: FkDependency.java

---
+++ daleq-core/src/main/java/de/brands4friends/daleq/core/internal/types/FkDependency.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012 brands4friends, Private Sale GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.brands4friends.daleq.core.internal.types;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+
+import de.brands4friends.daleq.core.TableType;
+
+public final class FkDependency {
+    private final TableType from;
+    private final TableType to;
+
+    public FkDependency(final TableType from, final TableType to) {
+        this.from = Preconditions.checkNotNull(from);
+        this.to = Preconditions.checkNotNull(to);
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hashCode(from, to);
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (obj instanceof FkDependency) {
+            final FkDependency that = (FkDependency) obj;
+
+            return Objects.equal(from, that.from)
+                    && Objects.equal(to, that.to);
+        }
+
+        return false;
+    }
+
+    @Override
+    public final String toString() {
+        return Objects.toStringHelper(this)
+                .add("from", from)
+                .add("to", to)
+                .toString();
+    }
+}
